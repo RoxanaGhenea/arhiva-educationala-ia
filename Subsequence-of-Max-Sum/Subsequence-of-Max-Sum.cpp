e9e66f4492b10238eb2a1889f1a38550161a2937
Roxana Ghenea <roxana_ghenea24@yahoo.com>
2022-12-08 15:50:23 +0000

    ssm changes addressed

--- Subsequence-of-Max-Sum/Subsequence-of-Max-Sum.cpp
+++ Subsequence-of-Max-Sum/Subsequence-of-Max-Sum.cpp
@@ -4,34 +4,34 @@
 
 using namespace std;
 int v[6000005];
-int Spv[6000005];
+int sPV[6000005];
 
 int main() 
 {
     ifstream fin("ssm.in");
     ofstream fout("ssm.out");
 
-    int N, Sp = 0, Smin = 0, Smax = -100000000, Ibeg = 1, Iend = 0, Ibegprint = 0;
+    int N, sP = 0, sMin = 0, sMax = -100000000, iBeg = 1, iEnd = 0, iBegPrint = 0;
 
     fin >> N;
     
     for (int i = 1; i <= N; ++i) {
         fin >> v[i];
-        Sp += v[i];
-        Spv[i] = Sp;
+        sP += v[i];
+        sPV[i] = sP;
     }
 
     for (int i = 1; i <= N; ++i) {
-        if (Spv[i] - Smin > Smax) {
-            Smax = Spv[i] - Smin;
-            Iend = i;
-            Ibegprint = Ibeg;
+        if (sPV[i] - sMin > sMax) {
+            sMax = sPV[i] - sMin;
+            iEnd = i;
+            iBegPrint = iBeg;
         }
-        if (Spv[i] < Smin) {
-            Smin = Spv[i];
-            Ibeg = i + 1;
+        if (sPV[i] < sMin) {
+            sMin = sPV[i];
+            iBeg = i + 1;
         }
     }
 
-    fout << Smax << " " << Ibegprint << " " << Iend;
+    fout << sMax << " " << iBegPrint << " " << iEnd;
 }
