b8ae00044900e39224367ad155bf822922c487c3
Roxana Ghenea <roxana_ghenea24@yahoo.com>
2022-12-08 17:28:17 +0000

    Second review change request addressed

--- k-Fibonacci/k-Fibonacci.cpp
+++ k-Fibonacci/k-Fibonacci.cpp
@@ -5,35 +5,35 @@
 using namespace std;
 int MOD = 666013;
 
-void matrixMultiplication (int X[2][2], int Y[2][2]) {
+void matrixMultiplication (int matrixX[2][2], int matrixY[2][2]) {
     int product[2][2] = {{0, 0}, {0, 0}};
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 2; ++j) {
             for (int k = 0; k < 2; ++k) {
-                product[i][j] = (int) ((product[i][j] + 1LL * X[i][k] * Y[k][j]) % MOD);
+                product[i][j] = (int) ((product[i][j] + 1LL * matrixX[i][k] * matrixY[k][j]) % MOD);
             }
         }
     }
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 2; ++j) {
-            X[i][j] = product[i][j];
+            matrixX[i][j] = product[i][j];
         }
     }
 }
 
-void expSquaring (int X[2][2], int k) {
+void power (int matrixX[2][2], int k) {
     int result[2][2] = {{1, 0}, {0, 1}};
     while (k) {
         if (k % 2 == 1) {
-            matrixMultiplication(result, X);
+            matrixMultiplication(result, matrixX);
         }
-        matrixMultiplication(X, X);
+        matrixMultiplication(matrixX, matrixX);
         k /= 2;
     }
 
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 2; ++j) {
-            X[i][j] = result[i][j];
+            matrixX[i][j] = result[i][j];
         }
     }
 }
@@ -42,11 +42,11 @@ int main()
 {
     ifstream fin ("kfib.in");
     ofstream fout ("kfib.out");
-    int Z[2][2] = {{0, 1}, {1, 1}};
+    int matrixZ[2][2] = {{0, 1}, {1, 1}};
     int k;
     fin >> k;
-    expSquaring (Z, k);
-    fout << Z[1][0];
+    power (matrixZ, k);
+    fout << matrixZ[1][0];
     return 0;
 }
 
