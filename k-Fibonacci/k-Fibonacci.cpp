309e6459cb735fd62d937ee900fb74a06a4692a6
Roxana Ghenea <roxana_ghenea24@yahoo.com>
2022-12-08 15:25:28 +0000

    Fibonacci changes addressed

--- k-Fibonacci/k-Fibonacci.cpp
+++ k-Fibonacci/k-Fibonacci.cpp
@@ -1,16 +1,14 @@
 // https://infoarena.ro/problema/kfib - Problem to be solved
 
-#include <iostream>
 #include <fstream>
 
 using namespace std;
 int MOD = 666013;
 
-void matrixMultiplication(int X[2][2], int Y[2][2]) {
+void matrixMultiplication (int X[2][2], int Y[2][2]) {
     int product[2][2] = {{0, 0}, {0, 0}};
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 2; ++j) {
-            product[i][j] = 0;
             for (int k = 0; k < 2; ++k) {
                 product[i][j] = (int) ((product[i][j] + 1LL * X[i][k] * Y[k][j]) % MOD);
             }
@@ -23,7 +21,7 @@ void matrixMultiplication(int X[2][2], int Y[2][2]) {
     }
 }
 
-void lg(int X[2][2], int k) {
+void expSquaring (int X[2][2], int k) {
     int result[2][2] = {{1, 0}, {0, 1}};
     while (k) {
         if (k % 2 == 1) {
@@ -47,7 +45,7 @@ int main()
     int Z[2][2] = {{0, 1}, {1, 1}};
     int k;
     fin >> k;
-    lg(Z, k);
+    expSquaring (Z, k);
     fout << Z[1][0];
     return 0;
 }
