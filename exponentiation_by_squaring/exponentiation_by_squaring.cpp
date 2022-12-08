19f0a85fb51a1b0f2c06d53eeb04a486fe7dfba3
Roxana Ghenea <roxana_ghenea24@yahoo.com>
2022-12-08 15:13:24 +0000

    Exponentiation by squring changes addressed

--- exponentiation_by_squaring/exponentiation_by_squaring.cpp
+++ exponentiation_by_squaring/exponentiation_by_squaring.cpp
@@ -1,20 +1,17 @@
-#include <iostream>
 #include <fstream>
 
 using namespace std;
 
 int x = 1999999973;
 
-int exponentiationBySquaring(long long a, long long b) {
-    int ans;
+int exponentiationBySquaring(int a, int b) {
     if (b == 0) {
-        ans = 1;
+        return 1;
     } else if (b % 2 == 0) {
-        ans = (exponentiationBySquaring((a * a) % x, b/2)) % x;
+        return exponentiationBySquaring((1LL * a * a) % x, b/2) % x;
     } else {
-        ans = (a * exponentiationBySquaring((a * a) % x, (b-1)/2)) % x;
+        return (1LL * a * exponentiationBySquaring((1LL * a * a) % x, (b-1)/2)) % x;
     }
-    return ans;
 }
 
 int main() 
