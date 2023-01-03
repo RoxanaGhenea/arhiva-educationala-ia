a735a5ca674ad90fe0d44d4cbc13b594933f158a
Roxana Ghenea <roxana_ghenea24@yahoo.com>
2023-01-03 21:51:51 +0000

    Stack data structure example.

---
+++ stdlib-data-structures/stack.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    stack<int> scores;
+    for (int i = 1; i <= 25; ++i) {
+        /* Below will insert 25 scores between 1 and 100 in the stack.
+        It will give every value that was inserted in the stack from the first to the last. */
+        int value = random() % 100 + 1;
+        scores.push(value);
+        cout << scores.top() << " ";
+    }
+
+    cout << "\n";
+
+    while (!scores.empty()) {
+        /* Below will record the last element, and the method pop will delete the last element
+        from the stack, cout will give the element that was deleted, this will continue until
+        the stack will be empty. */
+        int deletedElement = scores.top();
+        scores.pop();
+        cout << deletedElement << " ";
+    }
+}
