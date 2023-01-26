2dbb2d0fbb4b4222fb9d26e13a08be39fe0a408e
Roxana Ghenea <roxana_ghenea24@yahoo.com>
2023-01-26 17:34:13 +0000

    Contest 1236A comments addressed.

--- codeforces-contests/contest-1236A.cpp
+++ codeforces-contests/contest-1236A.cpp
@@ -12,16 +12,11 @@ int main()
         int heap1, heap2, heap3, numberStones = 0;
 
         cin >> heap1 >> heap2 >> heap3;
-        while (heap2 != 0 & heap3 >= 2) {
-            numberStones += 3;
-            heap2 -= 1;
-            heap3 -= 2;
-        }
-        while (heap1 != 0 & heap2 >= 2) {
-            numberStones += 3;
-            heap1 -= 1;
-            heap2 -= 2;
-        }
+        
+        numberStones = 3 * min(heap2, heap3 / 2);
+        heap2 -= min(heap2, heap3 / 2);
+        numberStones += 3 * min (heap1, heap2 / 2);
+        
         cout << numberStones << "\n";
     }
 }
