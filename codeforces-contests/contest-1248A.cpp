db27a5eeeb62a1ba18ca9d39505ee2da5482482e
Roxana Ghenea <roxana_ghenea24@yahoo.com>
2023-01-26 17:58:08 +0000

    Contest 1248A solution changes addressed.

--- codeforces-contests/contest-1248A.cpp
+++ codeforces-contests/contest-1248A.cpp
@@ -14,27 +14,29 @@ int main()
     cin >> test;
 
     for (int i = 1; i <= test; ++i) {
-        int n,m, countIntegerPoints = 0;
+        int n,m, oddP = 0, evenP = 0, oddQ = 0, evenQ = 0;
+        long long countIntegerPoints;
         cin >> n;
         for (int j = 1; j <= n; ++j) {
             cin >> p[j];
+            if (p[j] % 2 == 0) {
+                evenP += 1;
+            } else {
+                oddP += 1;
+            }
         }
         cin >> m;
         for (int k = 1; k <= m; ++k) {
             cin >> q[k];
-        }
-
-        for (int j = 1; j <= n; ++j) {
-            for (int k = 1; k <= m; ++k) {
-                double intersection;
-                intersection = (q[k] - p[j]) / 2.00;
-                cout << intersection << "\n";
-                if (intersection == int(intersection)) {
-                    countIntegerPoints += 1;
-                }
+            if (q[k] % 2 == 0) {
+                evenQ += 1;
+            } else {
+                oddQ += 1;
             }
         }
+
+        countIntegerPoints = 1LL * oddP * oddQ + 1LL * evenP * evenQ;
+
         cout << countIntegerPoints << "\n";
     }
-
 }
