6ce0ece2a8747b901e86ef13cd01bc2dce99a509
Roxana Ghenea <roxana_ghenea24@yahoo.com>
2023-01-26 18:33:28 +0000

    Contest 592 solution changes addressed.

--- codeforces-contests/contest-592A.cpp
+++ codeforces-contests/contest-592A.cpp
@@ -10,20 +10,19 @@ int main()
     int t;
     cin >> t;
     for (int i = 1; i <= t; ++i) {
-        int a, b, c, d, k;
-        double x, y;
+        int a, b, c, d, k, x, y;
 
         cin >> a >> b >> c >> d >> k;
         if (a % c == 0) {
             x = a / c;
         } else {
-            x = round(a/c + 0.5);
+            x = a / c + 1;
         }
 
         if (b % d == 0) {
             y = b / d;
         } else {
-            y = round(b/d + 0.5);
+            y = b / d + 1;
         }
         
         if (x + y <= k) {
