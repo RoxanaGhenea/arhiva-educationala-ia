9f33b766bd07f71082b40b1d2bc349752df295aa
Roxana Ghenea <roxana_ghenea24@yahoo.com>
2023-02-16 17:50:39 +0000

    wip

---
+++ bellmanford/bellmanford.cpp
@@ -0,0 +1,12 @@
+//https://infoarena.ro/problema/bellmanford
+
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream fin("bellamnford.in");
+    ofstream fout("bellmanford.out");
+    
+}
