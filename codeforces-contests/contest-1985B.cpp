4ad1313fac5d51b580071fb7832581b45140fcd0
RoxanaGhenea <roxana_ghenea24@yahoo.com>
2024-06-12 23:38:56 +0100

    contest 1985 B

---
+++ codeforces-contests/contest-1985B.cpp
@@ -0,0 +1,33 @@
+//https://codeforces.com/contest/1985/problem/B
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("contest-1985B.in", "r", stdin);
+    freopen("contest-1985B.out", "w", stdout);
+    #endif
+
+    int test;
+    cin >> test;
+
+    while(test--) {
+        int n, max = -1, quotient, sumMultiples, iMax;
+        cin >> n;
+
+        for (int i = 2; i <= n; ++i) {
+            quotient = n / i;
+            sumMultiples = i * quotient * (quotient + 1);
+            if (max <= sumMultiples) {
+                max = sumMultiples;
+                iMax = i;
+            }
+        }
+        cout << iMax << "\n";
+    }
+    
+}
